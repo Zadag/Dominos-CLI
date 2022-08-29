32e08cdc264914fc8d4a575e4c4e597c9d41385d
Kevin <kbrowne97@gmail.com>
2022-08-29 08:47:46 -0400

    add Jalopeno to test order

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,20 @@
 #! /usr/bin/env node
 
 console.log('hello world');
+
+//extra cheese thin crust pizza
+const pizza=new Item(
+    {
+        //16 inch hand tossed crust
+        code:'16SCREEN',
+        options:{
+            //sauce, whole pizza : normal
+            X: {'1/1' : '1'}, 
+            //cheese, whole pizza  : double 
+            C: {'1/1' : '1'},
+            //pepperoni, whole pizza : double 
+            P: {'1/2' : '1'},
+            J: {'1/1': '1'}
+        }
+    }
+);
